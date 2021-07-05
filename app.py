da5c07a1cec0ab21542ab4d17049394d6bc337ec
Visharad <visharadborsutkar777@gmail.com>
2021-07-06 03:44:16 +0530

    added major changes as follow : - update the navigation bar - added new animation upon scrolling in navbar - updated about section & skills - home section complete

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-posts = [
-    {
-        'author':'Visharad',
-        'title':'post-1',
-        'content':'first post',
-        'date_posted':'april 20'
-    },
-    {
-        'author':'John',
-        'title':'post-2',
-        'content':'second post',
-        'date_posted':'may 20'
-    }
-]
+skills = ["HTML", "CSS", "JavaScript", "Data Science", "OpenMP", "C++", "Python", "Fortran", "Deep Learning", "MPI"]
+styles = ["progress"+ str(i+1) for i in range(len(skills))]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts = posts)
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-@app.route("/projects")
-def projects():
-    return render_template('projects.html')
+    return render_template('home.html', skills = skills, length = len(skills), styles = styles)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -19,14 +19,13 @@ window.addEventListener('scroll', ()=>{
     })
 })*/
 // for typing animation
-const texts = ['Student', 'Developer', 'Freelancer'];
+const texts = ['Student. ', 'Developer. ', 'Freelancer. '];
 let count = 0;
 let index = 0;
 let currentText = "";
 let letter = "";
 
 (function type(){
-
     if(count === texts.length){
         count = 0;
     }
@@ -39,5 +38,34 @@ let letter = "";
         index = 0;
     }
     setTimeout(type, 350);
-
 }());
+
+const skills_array = ["HTML", "CSS", "JavaScript", "Data Science", "OpenMP", "C++", "Python", "Fortran", "Deep Learning", "MPI"];
+const skills_percentange = ["90", "80", "60", "70", "60", "90", "95", "70", "60", "60"];
+
+
+// for css property for perticular class 
+for(let i=0; i<skills_array.length; i++){
+    var x = document.getElementsByClassName("skill-name");
+    var num = i+1;
+    var num_str = num.toString();
+    var temp = "progress";
+    var z = temp.concat(num_str);
+    var y = document.getElementById(z.toString());
+    y.style.width = skills_percentange[skills_array.indexOf(x[i].getInnerHTML())] + "%";
+}
+
+// for navigation bar transition upon scrolling
+// function is triggered upon scrolling
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("navbar-id").style.background = "#272626";
+        document.getElementById("navbar-id").style.transitionDuration = "0.5s";
+    } else {
+        document.getElementById("navbar-id").style.background = 'transparent';
+        document.getElementById("navbar-id").style.transitionDuration = "0.5s";
+    }
+}
